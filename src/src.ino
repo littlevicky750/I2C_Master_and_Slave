7a45b336ec05bdcebd4aa1c0a4e9ed6445ade85f
Vicky Hung <120346234+littlevicky750@users.noreply.github.com>
2023-04-14 14:44:03 +0800

    Add files via upload

--- src/src.ino
+++ src/src.ino
@@ -8,17 +8,18 @@ const byte scl_M = 22;
 const byte sda_S = 4;
 const byte scl_S = 5;
 
-//#define Left_Plate
-
+#define Left_Plate
 // Left Plate Slave address = 0x0b
 // Right Plate Slave address = 0x09
 
 #ifdef Left_Plate
 #define I2C_DEV_ADDR_M 0x09
 #define I2C_DEV_ADDR_S 0x0b
+uint32_t dt = 1000;
 #else
 #define I2C_DEV_ADDR_M 0x0b
 #define I2C_DEV_ADDR_S 0x09
+uint32_t dt = 2000;
 #endif
 
 // Connect other I2C device to master for testing the function of the master.
@@ -26,10 +27,7 @@ const byte scl_S = 5;
 
 // ======================================================
 // TwoWire(0) work as Master
-
 TwoWire WireM = TwoWire(0);
-
-uint32_t m = 0;
 
 void Master_setup()
 {
@@ -47,19 +45,18 @@ void Master_loop()
 
   // Write message to the esp32 slave
   WireM.beginTransmission(I2C_DEV_ADDR_M);
-  WireM.printf("Hello  %u!", I2C_DEV_ADDR_M);
+  WireM.printf("Hello  %02x!", I2C_DEV_ADDR_M);
   uint8_t error = WireM.endTransmission(true);
   if (error != 0)
-    Serial.printf("[Master] %u endTransmission. error = %u\n", I2C_DEV_ADDR_M, error);
+    Serial.printf("[Master] 0x%02x endTransmission. error = %u\n", I2C_DEV_ADDR_M, error);
 
   // Read 16 bytes from the esp32 slave
   uint8_t bytesReceived = WireM.requestFrom(I2C_DEV_ADDR_M, 16);
   int start = millis();
-  Serial.printf("[Master] requestFrom 0x%u: %u\n", I2C_DEV_ADDR_M, bytesReceived);
+  Serial.printf("[Master] requestFrom 0x%02x: %u\n", I2C_DEV_ADDR_M, bytesReceived);
   if ((bool)bytesReceived)
   { // If received more than zero bytes
     uint8_t temp[bytesReceived];
-
     WireM.readBytes(temp, bytesReceived);
     for (int i = 1; i < bytesReceived; i++)
     {
@@ -80,7 +77,6 @@ void Master_loop()
 }
 // ====================================================================
 // TwoWire(1) work as slave
-
 TwoWire WireS = TwoWire(1);
 
 int s = 0;
@@ -89,7 +85,8 @@ void onRequest()
 {
   WireS.print(s);
   WireS.print(" Packets.");
-  Serial.print("[Slave] onRequest. print: ");
+
+  Serial.print("[Slave] onRequest. Print: ");
   Serial.print(s);
   Serial.println(" Packets.");
   s++;
@@ -97,7 +94,7 @@ void onRequest()
 
 void onReceive(int len)
 {
-  Serial.printf("[Slave] onReceive[%d]: ", len);
+  Serial.printf("[Slave] onReceive(len = %d): ", len);
   while (WireS.available())
   {
     Serial.write(WireS.read());
@@ -107,11 +104,9 @@ void onReceive(int len)
 
 void Slave_setup()
 {
-
   WireS.onReceive(onReceive);
   WireS.onRequest(onRequest);
   WireS.begin(I2C_DEV_ADDR_S, sda_S, scl_S, 0);
-
 #if CONFIG_IDF_TARGET_ESP32
   char message[64];
   snprintf(message, 64, "%u Packets.", s++);
@@ -132,5 +127,5 @@ void setup()
 void loop()
 {
   Master_loop();
-  delay(1000);
+  delay(dt);
 }
